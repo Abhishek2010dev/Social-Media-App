20ca0710aa2e3d51c281e978c7eeb0fbc9483d06
Abhishek2010dev <abhishekkumar2010dev@gmail.com>
2025-05-01 11:18:07 +0530

    shadcn/ui setup

--- app.vue
+++ app.vue
@@ -1,6 +1,9 @@
+<script setup lang="ts">
+import Button from "./components/ui/button/Button.vue";
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <Button>Hello, World</Button>
   </div>
 </template>
